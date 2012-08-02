4f41ec21d3af1b87d4b557e8423c35a29de29c1a
Makis Tracend <makis.tracend@gmail.com>
2012-08-02 13:10:30 -0700

    Adding section FB_Comments()

---
+++ helpers/fb_sections.php
@@ -0,0 +1,25 @@
+<?php
+
+class FB_Comments extends Section { 
+
+	function __construct($view=false, $vars=false, $data=false){
+		parent::__construct($view,$vars);
+		$this->data['options'] = $this->getOptions($data);
+		$this->render();
+	}
+
+	private function getOptions($data=false){
+		// currently options are not variable...
+		$options = array(
+				"href" => url($_SERVER["REQUEST_URI"]),
+				"width" => "600",
+				"colorscheme" => "light",
+				"num_posts" => "10"
+		);
+		
+		return $options;
+	}
+
+}
+
+?>
---
+++ views/sections/fb_comments.php
@@ -0,0 +1,3 @@
+<?
+echo '<div class="fb-comments" data-href="'. url($_SERVER["REQUEST_URI"]) .'" data-num-posts="'. $options['num_posts'] .'" data-width="'. $options['width'] .'" data-colorscheme="'. $options['colorscheme'] .'"></div>';
+?>
