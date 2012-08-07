265ca683e7b471131e338f0f863e3c96a5c459b3
Makis Tracend <makis.tracend@gmail.com>
2012-08-07 10:57:26 -0700

    Moving fb_appId to client.js

--- views/foot.php
+++ views/foot.php
@@ -1,5 +1,5 @@
 
 <div id="fb-root"><!-- --></div>
-<script type="text/javascript">var fb_appId = <?=$config['facebook']['appId']?>;</script>
+<script type="text/require">var fb_appId = <?=$config['facebook']['appId']?>;</script>
 <script type="text/javascript" src="http://connect.facebook.net/en_US/all.js" async="async"></script>
 <script type="text/javascript" src="<?=url("js/facebook.js")?>" async="async"></script>
