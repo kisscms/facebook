18889d83fe8275d668d6b46c24c0ec7fd19fd2ec
Makis Tracend <makis.tracend@gmail.com>
2012-08-09 12:14:34 -0700

    Updating attributes (data-type)

--- views/foot.php
+++ views/foot.php
@@ -1,8 +1,8 @@
 
 <div id="fb-root"><!-- --></div>
-<script type="text/require">
+<script type="text/javascript" data-type="require">
 	var fb_appId = <?=$config['facebook']['appId']?>;
-	var fb_uri = <?=$config['facebook']['uri']?>;
+	var fb_uri = "<?=$config['facebook']['uri']?>";
 </script>
 <script type="text/javascript" src="http://connect.facebook.net/en_US/all.js" async="async"></script>
 <script type="text/javascript" src="<?=url("js/facebook.js")?>" async="async"></script>
