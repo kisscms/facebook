e6d6ae69346efe8c5ca8982fb8a30ec00b550054
Makis Tracend <makis.tracend@gmail.com>
2012-08-08 17:56:53 -0700

    New method requestAccess()

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -2,12 +2,12 @@ window.fbAsyncInit = function() {
 	
 	
 	FB.init({
-	  appId      : fb_appId, // App ID
+	  appId 		: fb_appId, // App ID
 	  //channelUrl : '//'+window.location.hostname+'/channel', // Path to your Channel File
-	  status     : true, // check login status
-	  cookie     : true, // enable cookies to allow the server to access the session
-	  xfbml      : true,  // parse XFBML
-	  oauth : true 
+	  status 		: true, // check login status
+	  cookie 		: true, // enable cookies to allow the server to access the session
+	  xfbml 		: true,  // parse XFBML
+	  oauth 		: true 
 	});
 	
 	//console.log(FBcommon);
@@ -21,12 +21,39 @@ function FBcalls() {}
 
 FBcalls.prototype = {
 	
+	requestLogin: function(){
+	
+		FB.ui({
+		  method: 'oauth',
+		  client_id: fb_appId, 
+		  redirect_uri: 'https://apps.facebook.com/'+ fb_uri +'/'
+		},
+			function (response) {
+				if( typeof( response ) != "undefined") { 
+					if(response.session) {
+						//var user = JSON.parse(response.session);
+						// save the userid in the form
+						//$("#entry-form").find("input[name='fbid']").val(user.uid);
+						top.location.href = 'https://apps.facebook.com/'+ fb_uri +'/';
+					} else {
+						// No session
+						top.location.href = 'http://facebook.com/';
+					}
+				} else {
+					// denied access
+					top.location.href = 'http://facebook.com/';
+				}
+			}
+		);
+		
+	}, 
+	
 	addToPage: function() {
 
 		// calling the API ...
 		var obj = {
 		  method: 'pagetab',
-		  redirect_uri: 'http://apps.facebook.com/fb_appId/',
+		  redirect_uri: 'https://apps.facebook.com/'+ fb_uri +'/',
 		};
 		
 		FB.ui(obj);
