f8122e66d3c148ab0c4bf71de98d275a9909dc7b
Makis Tracend <makis.tracend@gmail.com>
2012-08-08 17:53:30 -0700

    Exporting uri(namespace) to client

--- views/foot.php
+++ views/foot.php
@@ -1,5 +1,8 @@
 
 <div id="fb-root"><!-- --></div>
-<script type="text/require">var fb_appId = <?=$config['facebook']['appId']?>;</script>
+<script type="text/require">
+	var fb_appId = <?=$config['facebook']['appId']?>;
+	var fb_uri = <?=$config['facebook']['uri']?>;
+</script>
 <script type="text/javascript" src="http://connect.facebook.net/en_US/all.js" async="async"></script>
 <script type="text/javascript" src="<?=url("js/facebook.js")?>" async="async"></script>
