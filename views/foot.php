89ae3204d3f574723586e85bad8287ca986d086c
Makis Tracend <makis.tracend@gmail.com>
2012-08-12 02:14:13 -0700

    Updating script includes

--- views/foot.php
+++ views/foot.php
@@ -1,8 +1,11 @@
-
 <div id="fb-root"><!-- --></div>
 <script type="text/javascript" data-type="require">
 	var fb_appId = <?=$config['facebook']['appId']?>;
 	var fb_uri = "<?=$config['facebook']['uri']?>";
+	(function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'http://connect.facebook.net/en_US/all.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	})();
 </script>
-<script type="text/javascript" src="http://connect.facebook.net/en_US/all.js" async="async"></script>
-<script type="text/javascript" src="<?=url("js/facebook.js")?>" async="async"></script>
+<script type="text/javascript" src="<?=url("js/facebook.js")?>" data-type="require"></script>
