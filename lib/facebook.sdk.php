2767d4b50fff81442380002c6b9bf3bc950f605e
Makis Tracend <makis.tracend@gmail.com>
2012-09-04 04:02:19 -0700

    Upgrading SDK support to v3.2.0

--- lib/facebook.sdk.php
+++ lib/facebook.sdk.php
@@ -2,6 +2,6 @@
 
 // Location of your Facebook SDK
 // - optionally define a different location for localhost and production
-include_once( SDK. "facebook/3.1.1/facebook.php");
+include_once( SDK. "facebook/3.2.0/facebook.php");
 
 ?>
