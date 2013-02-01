f9c8d12ad5c594e8a2f03a22121bc30da66177d0
Makis Tracend <makis.tracend@gmail.com>
2013-02-01 04:36:23 -0800

    #2 moving client vars to KISSCMS namespace

--- views/foot.php
+++ views/foot.php
@@ -3,11 +3,5 @@
 	// backwards compatibility...
 	var fb_appId = <?=$config['facebook']['appId']?>;
 	var fb_uri = "<?=$config['facebook']['uri']?>";
-	// new vars
-	var fb = {
-		key : "<?=$config['facebook']['appId']?>", 
-		uri : "<?=$config['facebook']['uri']?>",
-		token : <? echo ( empty($_SESSION['oauth']['facebook']['access_token']) ) ? false : '"'.$_SESSION['oauth']['facebook']['access_token'] .'"'; ?>
-	}
 </script>
 <script type="text/javascript" src="<?=url("/js/facebook.js")?>" data-type="require" data-path="facebook"></script>
--- views/head.php
+++ views/head.php
@@ -1,2 +1,7 @@
 <meta property="fb:app_id" content="<?=FB_APPID?>" />
 <meta property="fb:admins" content="<?=FB_ADMINS?>" />
+<?
+$GLOBALS['client']['fb']['key'] = $config['facebook']['appId'];
+$GLOBALS['client']['fb']['uri'] = $config['facebook']['uri'];
+$GLOBALS['client']['fb']['token']  = ( empty($_SESSION['oauth']['facebook']['access_token']) ) ? false : '"'.$_SESSION['oauth']['facebook']['access_token'] .'"';;
+?>
